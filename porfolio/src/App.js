75f724522e4c6091dcb1c6a29c1149706b461d7c
IvanTJ97 <100845085+IvanTJ97@users.noreply.github.com>
2022-06-22 11:14:36 +0200

    22/06

--- porfolio/src/App.js
+++ porfolio/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 export default ()=>{
   return <div className="container">
     <header>
-      <h2 id='TJ97'><a href="#">TJ97</a></h2>
+      <h2 id='TJ97'><a href="#">TJ97-Ivan Tejedor Farreras</a></h2>
       <nav>
         <ul>
           <li><a href="#projects">Projects</a></li>
@@ -13,50 +13,51 @@ export default ()=>{
     </header>
     <article className='head'>
       <div className="imagen">
-        <div className="cercle"></div>
+        <div className="cercle"/>
       </div>
       <div className="title">
-        <h1>Developer Front-End</h1>
-        <h4>Based in Barna</h4>
+        <h1>Front-End Developer</h1>
+        <h4>I love to think and develop web and mobile application ideas.</h4>
+        <h4>Based in Barcelona</h4>
         <h2>Skills</h2>
-        <h4>JS,CSS,HTML,REACT,STYLED COMPONENTS, REACT ROUTER, FIREBASE, GIT, JAVA, BOOTSTRAP</h4>
+        <h4>HTML, CSS, JavaScript, React, Styled-Components, React Router, Git, FireBase, NodeJS, React Native</h4>
       </div>
     </article>
     <article className='projects' id='projects'>
       <h2>Projects</h2>
       <section>
-        <h4>Section 1</h4>
-        <p>Description</p>
+        <h4>Profile</h4>
+        <p>This web actually, currently in development</p>
       </section>
       <section>
-        <h4>Section 2</h4>
-        <p>Description</p>
+        <h4>Oca</h4>
+        <p>Party Game, currently in development (80%)</p>
       </section>
       <section>
-        <h4>Section 3</h4>
-        <p>Description</p>
+        <h4>Tic, tac, toe</h4>
+        <p>Classical game</p>
       </section>
       <section>
-        <h4>Section 4</h4>
-        <p>Description</p>
+        <h4>Wordle</h4>
+        <p>Currently in development (50%)</p>
       </section>
     </article>
     <article className='about' id='about'>
-      <h2>About me</h2>
-        <p>About me 1</p>
-        <p>About me 2</p>
-        <p>About me 3</p>
-        <p>About me 4</p>
-        <p>About me 5</p>
+      <h2>Other achievements and random info</h2>
+      <p>Former student of physics at UB(University of Barcelona)</p>
+      <p>Graduated in international trade and in administration and finances.</p>
+      <p>Currently studying web and mobile programming, both front-end and back-end.</p>
+      <p>I would like to graduate from a computer science degree and learn other languages ​​such as java, python or golang.</p>
+      <p>Happily married and with a lovely dog.</p>
     </article>
     <article className='contact' id='contact'>
       <h2>Contact</h2>
       <ul>
-        <li>Tlf</li>
-        <li><input type="button" value="Email" class="button-1"/></li>
-        <li><input type="button" value="GitHub" class="button-1"/></li>
+        <li><input type="button" value="610218409" className="button-1"/></li>
+        <li><input type="button" value="ivan.tejfar@gmail.com" className="button-1"/></li>
+        <li><input type="button" value="GitHub" className="button-1"/></li>
       </ul>
     </article>
-    <footer>Cosecha propria... aquí hay patacas</footer>
+    <footer>Cosecha propria... aquí hay patacas v0.0.1</footer>
   </div>
 }
