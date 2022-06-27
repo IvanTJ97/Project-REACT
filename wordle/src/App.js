44460f70203e8ab0b6130a42a34b6f12beb124da
IvanTJ97 <100845085+IvanTJ97@users.noreply.github.com>
2022-06-27 11:25:12 +0200

    27/06

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import {useState} from 'react';
+import {Container} from './Style';
+import {useState,useEffect} from 'react';
+import { getItems } from './app/api';
 const App=()=>{
+  const [PALABRA,setPALABRA]=useState([]);
+  const [visible,setVisible]=useState(false);
+  const [visible2,setVisible2]=useState(false);
   const [pos,setPos]=useState(1);
   const [fila,setFila]=useState(1);
+  const TECLADO=["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Ñ","Z","X","C","V","B","N","M"];
+  const [palabras,setPalabras]=useState([]);
   const [array, setArray] = useState([
-    {value:"",class:"green"}, 
-    {value:"",class:"red"}, 
-    {value:"",class:"yellow"}, 
+    {value:"",class:""}, 
+    {value:"",class:""}, 
+    {value:"",class:""}, 
     {value:"",class:""}, 
     {value:"",class:""}, 
     {value:"",class:""}, 
@@ -35,9 +42,15 @@ const App=()=>{
     {value:"",class:""}, 
     {value:"",class:""}
   ]);
-  const TECLADO=["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Ñ","Z","X","C","V","B","N","M"];
-  const PRUEBA="MAREO";
-  console.log(PRUEBA.split(""));
+  useEffect(()=>{
+    getItems()
+      .then(res=>setPalabras(res))
+  },[]);
+  const generarPalabra=()=>{
+    const PRUEBA=palabras[Math.floor(Math.random()*12)].value;
+    setPALABRA(PRUEBA.split(""));
+    setVisible(true);
+  }
   const marcar=input=>{
     if(fila===1){
       (pos===1)?array[0].value=input:
@@ -90,7 +103,6 @@ const App=()=>{
     (pos>5)?setPos(6):setPos(pos+1);
   };
   const borrar=()=>{
-
     if(fila===1){
       (pos===2)?array[0].value="":
       (pos===3)?array[1].value="":
@@ -136,41 +148,170 @@ const App=()=>{
     (pos===1)?setPos(1):setPos(pos-1);
   }
   const enter=()=>{
+    let answer,points=0,rest=[...PALABRA];
     if(fila===1&&array[4].value!==""){
+      answer=[array[0].value,array[1].value,array[2].value,array[3].value,array[4].value];
+      for(let i=0;i<5;i++){
+        if(array[i].value===PALABRA[i]){
+          array[i].class="green";
+          answer[i]="";
+          rest[i]="";
+          points++;
+        }
+        else array[i].class="red";
+      };
+      for(let i=0;i<5;i++){
+        for(let j=0;j<5;j++){
+          if(answer[i]!==""&&answer[i]===rest[j]){
+            array[i].class="yellow";
+            answer[i]="";
+            rest[j]="";
+          }
+        }
+      };
+      (points===5)?ganar():points=0;
       setFila(fila+1);
       setPos(1);
     }
     else if(fila===2&&array[9].value!==""){
+      answer=[array[5].value,array[6].value,array[7].value,array[8].value,array[9].value];
+      for(let i=0;i<5;i++){
+        if(array[i+5].value===PALABRA[i]){
+          array[i+5].class="green";
+          answer[i]="";
+          rest[i]="";
+          points++;
+        }
+        else array[i+5].class="red";
+      };
+      for (let i=0;i<5;i++){
+        for(let j=0;j<5;j++){
+          if(answer[i]!==""&&answer[i]===rest[j]) {
+            array[i+5].class="yellow";
+            answer[i]="";
+            rest[j]="";
+          }
+        }
+      };
+      (points===5)?ganar():points=0;
       setFila(fila+1);
       setPos(1);
     }
     else if(fila===3&&array[14].value!==""){
+      answer=[array[10].value,array[11].value,array[12].value,array[13].value,array[14].value];
+      for(let i=0;i<5;i++){
+        if(array[i+10].value===PALABRA[i]) {
+          array[i+10].class="green";
+          answer[i]="";
+          rest[i]="";
+          points++;
+        }
+        else array[i+10].class="red";
+      };
+      for(let i=0;i<5;i++){
+        for(let j=0;j<5;j++){
+          if(answer[i]!==""&&answer[i]===rest[j]){
+            array[i+10].class="yellow";
+            answer[i]="";
+            rest[j]="";
+          }
+        }
+      };
+      (points===5)?ganar():points=0;
       setFila(fila+1);
       setPos(1);
     }
     else if(fila===4&&array[19].value!==""){
+      answer=[array[15].value,array[16].value,array[17].value,array[18].value,array[19].value];
+      for(let i=0;i<5;i++){
+        if(array[i+15].value===PALABRA[i]){
+          array[i+15].class="green";
+          answer[i]="";
+          rest[i]="";
+          points++;
+        }
+        else array[i+15].class="red";
+      };
+      for(let i=0;i<5;i++){
+        for(let j=0;j<5;j++) {
+          if(answer[i]!==""&&answer[i]===rest[j]){
+            array[i+15].class="yellow";
+            answer[i]="";
+            rest[j]="";
+          }
+        }
+      };
+      (points===5)?ganar():points=0;
       setFila(fila+1);
       setPos(1);
     }
     else if(fila===5&& array[24].value!==""){
+      answer=[array[20].value,array[21].value,array[22].value,array[23].value,array[24].value];
+      for(let i=0;i<5;i++){
+        if (array[i+20].value===PALABRA[i]){
+          array[i+20].class="green";
+          answer[i]="";
+          rest[i]="";
+          points++;
+        }
+        else array[i+20].class="red";
+      };
+      for(let i=0;i<5;i++){
+        for(let j=0;j<5;j++) {
+          if (answer[i]!==""&&answer[i]===rest[j]){
+            array[i+20].class="yellow";
+            answer[i]="";
+            rest[j]="";
+          }
+        }
+      };
+      (points===5)?ganar():points=0;
       setFila(fila+1);
       setPos(1);
     }
     else if(fila===6&&array[29].value!==""){
+      answer=[array[25].value,array[26].value,array[27].value,array[28].value,array[29].value];
+      for(let i=0;i<5;i++){
+        if (array[i+25].value===PALABRA[i]){
+          array[i+25].class="green";
+          answer[i]="";
+          rest[i]="";
+          points++;
+        }
+        else array[i+25].class = "red";
+      };
+      for(let i=0;i<5;i++){
+        for(let j=0;j<5;j++){
+          if(answer[i]!==""&&answer[i]===rest[j]){
+            array[i+25].class="yellow";
+            answer[i]="";
+            rest[j]="";
+          }
+        }
+      };
+      (points===5)?ganar():points=0;
       setFila(fila+1);
       setPos(1);
     }
+  };
+  const ganar=()=>{
+    alert("HAS GANADO");
+    window.location.reload(false);
   }
-  return <div className="container">
-      <div className="tablero">
+  return <Container visible={visible}>
+      <div className="inicio" visible={visible}>
+        <input type="button" value="COMENZAR" onClick={generarPalabra}/>
+        <h1>¡HAS GANADO!</h1>
+      </div>
+      <div className="tablero" visible={visible}>
         {array.map((obj,i)=><div key={i} className={obj.class}>{obj.value}</div>)}
       </div>
-      <div className="teclado">
+      <div className="teclado" visible={visible}>
         {TECLADO.map((obj,i)=><div key={i} onClick={()=>{marcar(`${obj}`)}}>{obj}</div>)}
         <div className="blanco"></div>
         <div onClick={()=>{borrar()}}>Borrar</div>
         <div onClick={()=>{enter()}}>Enter</div>
       </div>
-    </div>
+    </Container>
 };
 export default App;
---
+++ wordle/src/Style.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+export const Container=styled.div`
+  display: ${({visible})=>visible?"flex":"grid"};
+  justify-content: space-evenly;
+  place-items:center;
+  margin-top: 1%;
+.inicio{
+    display:${({visible})=>visible?"none":"grid"};
+    place-items:center;
+    height:100vh;
+    width:100vh;
+}
+input{
+    padding:25px;
+    background-color:#f0c;
+    cursor: pointer;
+    display:${({visible2})=>visible2?"none":"block"};
+}
+h1{
+  display:${({visible2})=>visible2?"block":"none"};
+}
+.teclado,.tablero{
+  display: ${({visible})=>visible?"grid":"none"};
+  gap: 10px;
+  width: 45%;
+  grid-template-columns: repeat(5, 17%);
+  grid-template-rows: repeat(6, 80px);
+}
+.tablero div,.teclado div:not(.blanco) {
+  border: 1px solid #000;
+  display: grid;
+  place-items: center;
+}
+.teclado div:hover:not(.blanco) {
+  cursor: pointer;
+  background: #ccc;
+}
+.green{
+  background-color: #0f0;
+}
+.red{
+  background-color: #f00;
+}
+.yellow{
+  background-color: #ff0;
+}
+`
